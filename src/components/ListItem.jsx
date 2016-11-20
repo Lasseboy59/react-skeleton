ed2b73b9739f45f52e655eb68fa9e010c6ce8016
Lasseboy59 <lauri.kyttala@gmail.com>
2016-11-20 15:06:47 +0200

    react-skeleton

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,12 @@
+var React = request('react');
+var ListItem = React.createClass({
+  render: function() {
+    return (
+      <li>
+        <h4>{this.props.ingredient}</h4>
+      </li>
+    );
+  }
+});
+
+module.exports = ListItem;
